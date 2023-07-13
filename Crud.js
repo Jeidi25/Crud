309686fa26f2831b50033a8092197e031321bb50
Jeidi25 <jeidibaron@gmail.com>
2023-07-13 12:10:18 -0500

    Guardar nombres

--- Crud.js
+++ Crud.js
@@ -9,3 +9,20 @@ botonAgregar.addEventListener("click", ()=>{
     modal.classList.remove("oculto")
 })
 
+function Datos (Nombre, Apellido, Email, Tipo){
+
+    this.Nombre = Nombre
+    this.Apellido = Apellido
+    this.Email = Email
+    this.Tipo = Tipo
+}
+
+const datos = [];
+
+function obtenerNombre ()
+
+const Datos = document.querySelector (".Nombre")
+obtenerNombre = parseInt (obtenerNombre);
+
+return obtenerNombre
+
