fe67adec516e435b6044ac0a9c4fe09d6aa233d8
Jeidi25 <jeidibaron@gmail.com>
2023-07-17 16:52:25 -0500

    actualización de tablas

--- Crud.js
+++ Crud.js
@@ -1,4 +1,5 @@
 const modal = document.querySelector(".modal")
+const lista = []
 const buttonSalir = modal.querySelector(".Salir")
 buttonSalir.addEventListener("click", ()=>{
     modal.classList.add("oculto")
@@ -11,27 +12,56 @@ botonAgregar.addEventListener("click", ()=>{
 
 const Guardar = document.querySelector(".Guardar")
 Guardar.addEventListener("click",()=>{
+    guadarDatos()
     modal.classList.add("oculto")
 })
-function Datos (Nombre, Apellido, Email, Tipo){
 
-    this.Nombre = Nombre
-    this.Apellido = Apellido
-    this.Email = Email
-    this.Tipo = Tipo
+function guadarDatos(){
+    const Nombre = document.querySelector("#inputNombre").value
+    const Apellido = document.querySelector("#inputApellido").value
+    const Email = document.querySelector("#inputEmail").value
+    const Tipo = document.querySelector("#inputTipo").value
 
-    const Date = [];
+    if(Nombre.trim() == "" ) alert("El nombre no puede estar vacio")
+    if(Nombre.trim() == "" ) alert("El nombre no puede estar vacio")
+    if(Nombre.trim() == "" ) alert("El nombre no puede estar vacio")
+    if(Nombre.trim() == "" ) alert("El nombre no puede estar vacio")
 
-    function obtenerNombre (){
+    const camposValidos = [Nombre, Apellido, Email, Tipo].every(x => x.trim() !== "")
+    if(!camposValidos) alert("Los campos no son validos")
 
-    }
-
-    const Datos = document.querySelector (".Nombre")
-    obtenerNombre = parseInt (obtenerNombre);
-
-    return obtenerNombre;
+    lista.push({
+        Nombre: Nombre, 
+        Apellido: Apellido,
+        Email : Email,
+        Tipo : Tipo,
+     
+    })
+    console.log(lista)
+    actualizarTabla()
 }
 
+function actualizarTabla() {
+    const tablaBody = document.querySelector(".tabla tbody")
+    let tableBodyContenido = ""
+    for(let i = 0; i<lista.length; i++){
+        tableBodyContenido += ` 
+        <tr>
+            <td>${lista[i].Nombre}</td>
+            <td>${lista [i].Apellido}</td>
+            <td>${lista[i].Email}</td>
+            <td>${lista[i].Tipo}</td>
+            <td>    
+                <button class="fa fa-trash"></button>
+            </td>
+        </tr>
+        `
+    }
 
+    tablaBody.innerHTML = tableBodyContenido
+
+
+
+}
 
 
