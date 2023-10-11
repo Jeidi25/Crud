13438a039d1823462f7d8fc4017c4580a2ad5103
Jeidi25 <jeidibaron@gmail.com>
2023-10-11 11:36:12 -0500

    Version 2.0

--- Crud.js
+++ Crud.js
@@ -5,7 +5,6 @@ botonAgregar.addEventListener('click', function () {
     formulario.style.display = 'flex';
 });
 
-
 const botonSalir = document.querySelector('.Salir');
 botonSalir.addEventListener('click', function () {
 formulario.style.display = 'none';
@@ -19,12 +18,10 @@ const apellidoInput = document.getElementById('Apellido').value;
 const emailInput = document.getElementById('E-mail').value;
 const tipoSelect = document.getElementById('Tipo').value;
 
-const formulario = document.querySelector(".formulario");
-
+const table = document.querySelector(".Table");
 const nuevaClass = document.createElement('field');
-
 const divActivo = document.createElement('field');
-divActivo.innerText = 'x'
+divActivo.innerText = ' '
 const divNombre = document.createElement('field');
 divNombre.innerText = nombreInput;
 const divApellido = document.createElement('field');
@@ -40,7 +37,34 @@ nuevaClass.appendChild(divApellido);
 nuevaClass.appendChild(divEmail);
 nuevaClass.appendChild(divTipo);
 
-formulario.appendChild(nuevaClass);
+table.appendChild(nuevaClass);
 
 formulario.style.display = 'none';
+
 });
+const Anterior= document.querySelector('.Anterior')
+Siguiente.addEventListener('click', function (){
+});
+
+const Siguiente= document.querySelector('.Siguiente')
+Siguiente.addEventListener('click', function (){
+
+});
+
+const divActivo = document.createElement("Activo");
+divActivo.classList = "Tipo";
+
+const divUsuario =document.createElement("Usuarios")
+
+var celda1 = document.createElement("div");
+celda1.className = "celda";
+celda1.textContent = "Nuevo Dato 1";
+
+var celda2 = document.createElement("div");
+celda2.className = "celda";
+celda2.textContent = "Nuevo Dato 2";
+
+var celda3 = document.createElement("div");
+celda3.className = "celda";
+celda3.textContent = "Nuevo Dato 3";
+
